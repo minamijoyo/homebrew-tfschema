d0a0038527c86da0952fc0736a9dac9559eff3a2
Masayuki Morita <minamijoyo@gmail.com>
2018-07-27 18:48:38 +0900

    Brew formula update for tfschema version v0.1.2

--- tfschema.rb
+++ tfschema.rb
@@ -1,9 +1,9 @@
 class Tfschema < Formula
   desc "A schema inspector for Terraform providers."
   homepage "https://github.com/minamijoyo/tfschema"
-  url "https://github.com/minamijoyo/tfschema/releases/download/v0.1.1/tfschema_0.1.1_darwin_amd64.tar.gz"
-  version "0.1.1"
-  sha256 "8af12ad08fb9c814adbcc7560fe700cd3e94e93cc0900d10cbd001746c873439"
+  url "https://github.com/minamijoyo/tfschema/releases/download/v0.1.2/tfschema_0.1.2_darwin_amd64.tar.gz"
+  version "0.1.2"
+  sha256 "62e8364ce9c9a3f4c38d88847156eee1c2b76c74a80b7c69990d361f0ce54377"
 
   def install
     bin.install "tfschema"
